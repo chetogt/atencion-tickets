e49a41832f0dd364ee17ba91f12ac5cc8d16d8b6
José Córdova <ja.cordovap@gmail.com>
2018-11-30 12:46:12 -0600

    Se agregan métodos de consulta

--- src/main/java/gt/com/kinal/tickets/PruebaTickets.java
+++ src/main/java/gt/com/kinal/tickets/PruebaTickets.java
@@ -8,10 +8,15 @@ package gt.com.kinal.tickets;
 import gt.com.kinal.tickets.model.AtTicket;
 import gt.com.kinal.tickets.model.AtTecnico;
 import java.util.Date;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 /**
  *
@@ -25,6 +30,7 @@ public class PruebaTickets {
         
         tx.begin();
         // todo lo que se encuentra acá está dentro de la transacción
+        /*
         // INSERT
         AtTicket ticket = new AtTicket("Navegacion", "No puedo ingresar a pagina de Banguat (DEL)", "C", new Date());
         em.persist(ticket);
@@ -36,7 +42,43 @@ public class PruebaTickets {
         ticket.setEstado("A");
         
         // DELETE
-        em.remove(ticket);
+        em.remove(ticket);*/
+        
+        // consulta utilizando NamedQueries
+        Query queryTickets = em.createNamedQuery("AtTicket.findAll");
+        List<AtTicket> tickets = queryTickets.getResultList();
+        System.out.println("Tickets encontrados: " + tickets.size());
+        
+        // consulta utilizando dinamic queries
+        String strQuery = "SELECT a FROM AtTicket a WHERE ";
+        String strWhere = "a.estado = :estado";
+        Long tecnicoId = 1L;
+        if(tecnicoId != null) {
+            strWhere += " AND a.tecnico.tecnicoId = :tecnicoId";
+        }
+        Query queryTicketsCreados = em.createQuery(strQuery + strWhere);
+        queryTicketsCreados.setParameter("estado", "A");
+        if(tecnicoId != null) {
+            queryTicketsCreados.setParameter("tecnicoId", tecnicoId);
+        }
+        List<AtTicket> ticketsCreados = queryTicketsCreados.getResultList();
+        System.out.println("Tickets creados: " + ticketsCreados.size());
+        
+        // consulta utilizando sql nativo
+        String strQueryDos = "SELECT * FROM at_ticket WHERE estado = 'A'";
+        Query queryTicketsAsignados = em.createNativeQuery(strQueryDos, AtTicket.class);
+        List<AtTicket> ticketsAsignados = queryTicketsAsignados.getResultList();
+        System.out.println("Tickets asignados: " + ticketsAsignados.size());
+        
+        // consulta utilizando criteria api
+        CriteriaBuilder builder = em.getCriteriaBuilder();
+        CriteriaQuery<AtTicket> criteriaQuery = builder.createQuery(AtTicket.class);
+        Root<AtTicket> t = criteriaQuery.from(AtTicket.class);
+        criteriaQuery.select(t).where(builder.equal(t.get("estado"), "S"));
+        
+        Query queryTicketsFallidos = em.createQuery(criteriaQuery);
+        List<AtTicket> ticketsFallidos = queryTicketsFallidos.getResultList();
+        System.out.println("Tickets fallidos: " + ticketsFallidos.size());
         
         tx.commit();
         System.out.println("Transaccion ejecutada");
