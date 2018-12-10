39ee7ed869ebffa996e79198be8c47cb81ec4a81
José Córdova <ja.cordovap@gmail.com>
2018-12-10 12:02:47 -0600

    Se agrega schedule programatico

---
+++ src/main/java/gt/com/kinal/tickets/service/ComprobarTicketsNoAsignados.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gt.com.kinal.tickets.service;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.ejb.Timeout;
+import javax.ejb.Timer;
+import javax.ejb.TimerService;
+
+/**
+ *
+ * @author jose
+ */
+@Singleton
+@Startup
+public class ComprobarTicketsNoAsignados {
+    @Resource
+    TimerService timerService;
+
+    @PostConstruct
+    public void initialize() {
+        timerService.createTimer(0, 10000, "TICKETS_NO_ASIGNADOS");
+    }
+
+    @Timeout
+    public void programmaticTimeout(Timer timer) {
+        System.out.println("Comprobando tickets no asignados...");
+    }
+}
